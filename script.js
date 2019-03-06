c92ba8c20c55656db196d008e8bdc12eb8e0f01f
Noel-SG <45712638+Noel-SG@users.noreply.github.com>
2019-03-06 21:07:48 +0100

    Highlighting
    Added the highlights of element categories

--- script.js
+++ script.js
@@ -1,9 +1,87 @@
-$(document).ready(function(){
-    $("#hideGuide").click(function(){
-      $("columnRight-container").hide();
+jQuery(document).ready(function($){
+    console.log("Begin");
+    $("#hideGuide").on("click", function(){
+      $(".columnRight-container").slideToggle(500);
     });
     
-    $("#showGuide").click(function(){
-      $("columnRight-containe").show();
+    $("#showGuide").on("click", function(){
+      $(".columnRight-container").toggle();
     });
-  });
+
+    $(".transitionMetalsKey").hover(function(){
+        $(".transitionMetals").toggleClass("elementHover");
+        console.log("Transition Metals Activated");
+    })
+
+    $(".alkaliMetalKey").hover(function(){
+        $(".alkaliMetals").toggleClass("elementHover");
+        
+    })
+
+    $(".lanthanidesKey").hover(function(){
+        $(".lanthanides").toggleClass("elementHover");
+        
+    })
+
+    $(".actinoidsKey").hover(function(){
+        $(".actinoids").toggleClass("elementHover");
+        
+    })
+
+    $(".othersKey").hover(function(){
+        $(".others").toggleClass("elementHover");
+        
+    })
+
+    $(".alkalineMetalKey").hover(function(){
+        $(".alkalineMetals").toggleClass("elementHover");
+        
+    })
+
+    $(".metalloidsKey").hover(function(){
+        $(".metalloids").toggleClass("elementHover");
+        
+    })
+
+    $(".nonMetalsKey").hover(function(){
+        $(".nonMetals").toggleClass("elementHover");
+        
+    })
+
+    $(".othersKey").hover(function(){
+        $(".others").toggleClass("elementHover");
+        
+    })
+
+    $(".metalKey").hover(function(){
+        $(".metals").toggleClass("elementHover");
+        
+    })
+
+    $(".nobleKey").hover(function(){
+        $(".noblegas").toggleClass("elementHover");
+        
+    })
+
+    $(".halideKey").hover(function(){
+        $(".halide").toggleClass("elementHover");
+        
+    })
+
+    $(".GR1").hover(function(){
+        $(".gr1").toggleClass("elementHover");
+        console.log("Gr1 highlight");
+    })
+    
+    
+
+    /*$(".row.guide-row").hover(function(e){
+        var x = $(e.target).data('activation')
+
+        $("." + x).toggleClass("elementHover");       
+        console.log()
+    })
+
+   */
+    
+});
